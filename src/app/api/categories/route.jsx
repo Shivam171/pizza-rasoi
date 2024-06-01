36cdaeb037a0e045ed82fcf2d375a89cb113f119
shivam171 <shiv.op@gmail.com>
2024-06-01 15:29:53 +0530

    Added Categories Page under User Tabs

---
+++ src/app/api/categories/route.jsx
@@ -0,0 +1,19 @@
+import { Category } from "@/app/models/Category";
+
+export async function POST(req) {
+  const { name } = await req.json();
+  const categoryDoc = await Category.create({name});
+  return Response.json(categoryDoc);
+}
+
+export async function PUT(req) {
+  const {_id, name} = await req.json();
+  await Category.updateOne({_id}, {name});
+  return Response.json(true);
+}
+
+export async function GET(){
+  return Response.json(
+    await Category.find()
+  )
+}
--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,10 +1,26 @@
 'use client'
 import useProfile from "@/components/UseProfile";
 import UserTabs from "@/components/layout/UserTabs";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
-
+  const [categoryName, setCategoryName] = useState('');
   const { loading: profileLoading, data: profileData } = useProfile();
+  const [categories, setCategories] = useState([]);
+  const [editedCategory, setEditedCategory] = useState(null);
+
+  useEffect(() => {
+    fetchCategories();
+  }, [])
+
+  function fetchCategories() {
+    fetch('/api/categories').then(res => {
+      res.json().then(categories => {
+        setCategories(categories);
+      })
+    })
+  }
 
   if (profileLoading) {
     return 'Loading user info...';
@@ -14,9 +30,80 @@ export default function CategoriesPage() {
     return 'Not an admin';
   }
 
+  async function handleCategorySubmit(ev) {
+    ev.preventDefault();
+    console.log("New category name:", categoryName);
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const data = { name: categoryName };
+      if (editedCategory) {
+        data._id = editedCategory._id;
+      }
+      const response = await fetch('/api/categories', {
+        method: editedCategory ? 'PUT' : 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      });
+      fetchCategories();
+      if (response.ok) {
+        console.log("Category created successfully");
+        resolve();
+      }
+      else {
+        console.error("Failed to create category!");
+        reject();
+      }
+    })
+    await toast.promise(creationPromise, {
+      loading: editedCategory
+        ? 'Updating category...'
+        : 'Creating you new category...',
+      success: editedCategory
+        ? 'Category updated'
+        : 'Category created',
+      error: 'Error, sorry...',
+    })
+    setCategoryName('');
+    setEditedCategory(null);
+  }
+
   return (
     <section className="mt-8 max-w-lg mx-auto">
       <UserTabs isAdmin={true} />
+      <form className="mt-8" onSubmit={handleCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>
+              {editedCategory ? 'Update Category' : 'New category name'}
+              {editedCategory && (
+                <>: <b>{editedCategory.name}</b></>
+              )}
+            </label>
+            <input
+              type="text"
+              value={categoryName}
+              onChange={ev => setCategoryName(ev.target.value)}
+            />
+          </div>
+          <div className="pb-4">
+            <button type="submit" className="border border-primary">
+              {editedCategory ? 'Update' : 'Create'}
+            </button>
+          </div>
+        </div>
+      </form>
+      <div>
+        <h2 className="mt-8 text-sm text-gray-500">Edit category:</h2>
+        {categories?.length > 0 && categories.map(c => (
+          <button
+            onClick={() => {
+              setEditedCategory(c);
+              setCategoryName(c.name);
+            }}
+            className="bg-gray-200 rounded-xl p-2 px-4 flex gap-1 cursor-pointer mb-1 border border-gray-300" key={c._id}>
+            <span>{c.name}</span>
+          </button>
+        ))}
+      </div>
     </section>
   )
 }
