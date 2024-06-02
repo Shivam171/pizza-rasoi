c6db0ce631190440d2d67756487f9dcbf9cf2e9f
shivam171 <shiv.op@gmail.com>
2024-06-02 10:20:36 +0530

    Updated Menu Items Page

--- src/app/api/menu-items/route.jsx
+++ src/app/api/menu-items/route.jsx
@@ -1,8 +1,16 @@
+import { MenuItems } from "@/app/models/MenuItem";
 import mongoose from "mongoose";
 
-export async function POST(req){
+export async function POST(req) {
   mongoose.connect(process.env.MONGO_URL)
   const data = await req.json();
-  const menuItemDoc = await MenuItem.create(data);
+  const menuItemDoc = await MenuItems.create(data);
   return Response.json(menuItemDoc)
+}
+
+export async function GET(req) {
+  mongoose.connect(process.env.MONGO_URL)
+  return Response.json(
+    await MenuItems.find()
+  )
 }
